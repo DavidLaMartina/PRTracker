fd7c13633c417c1bee6debe82908d4623fe190ab
David LaMartina <david.jos.lam@gmail.com>
2022-11-27 20:17:12 -0600

    Using chewie video player wrapper for a nice video UI.

--- lib/widgets/video_player_wrapper.dart
+++ lib/widgets/video_player_wrapper.dart
@@ -1,6 +1,6 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:prtracker/models/record.dart';
 import 'package:prtracker/services/local_media_service.dart';
 import 'package:video_player/video_player.dart';
 
@@ -17,56 +17,60 @@ class VideoPlayerWrapper extends StatefulWidget {
 class _VideoPlayerWrapperState extends State<VideoPlayerWrapper> {
   final LocalMediaService _localMediaService = GetIt.I.get();
   late VideoPlayerController _videoController;
+  late ChewieController _chewieController;
   late Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
     super.initState();
-    // https://pub.dev/packages/video_player
-    // https://docs.flutter.dev/cookbook/plugins/play-video
-    final videoFile = _localMediaService.openFileFromDisk(widget.videoUri);
-    _videoController = VideoPlayerController.file(videoFile);
+    _videoController = VideoPlayerController.file(
+        _localMediaService.openFileFromDisk(widget.videoUri));
     _initializeVideoPlayerFuture = _videoController.initialize();
+    // We're waiting until the build to set up the chewie controller
+    // because until the video player control inits, we don't have access
+    // to the true aspect ratio, which the Chewie Controller requires
+    // TODO: Write better version of chewie controller
+  }
+
+  void _setupChewieController() {
+    _chewieController = ChewieController(
+        videoPlayerController: _videoController,
+        aspectRatio: _videoController.value.aspectRatio,
+        autoInitialize: false,
+        autoPlay: false,
+        looping: false,
+        errorBuilder: ((context, errorMessage) {
+          return Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(errorMessage)));
+        }));
+    _chewieController.setVolume(0);
   }
 
   @override
   void dispose() {
     _videoController.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: ((context, snapshot) {
-            return Center(child: videoPlayer(context, snapshot));
-          })),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              if (_videoController.value.isPlaying) {
-                _videoController.pause();
-              } else {
-                _videoController.play();
-              }
-            });
-          },
-          child: Icon(_videoController.value.isPlaying
-              ? Icons.pause
-              : Icons.play_arrow)),
-    );
+        body: FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              _setupChewieController();
+              return Center(child: videoPlayer(context, snapshot));
+            }));
   }
 
   Widget videoPlayer(BuildContext context, AsyncSnapshot<void> snapshot) {
     if (snapshot.connectionState == ConnectionState.done) {
-      return AspectRatio(
-        aspectRatio: _videoController.value.aspectRatio,
-        child: VideoPlayer(_videoController),
-      );
+      return Chewie(controller: _chewieController);
     } else {
-      return const Center(child: CircularProgressIndicator());
+      return const CircularProgressIndicator();
     }
   }
 }
